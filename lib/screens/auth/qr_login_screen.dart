5d51de939a3ae759bc73db5c70e0d3d19146ff41
ale-russ <agarrussom@gmail.com>
2025-03-05 00:17:59 +0300

    working on qr_logging in

--- lib/screens/auth/qr_login_screen.dart
+++ lib/screens/auth/qr_login_screen.dart
@@ -1,10 +1,11 @@
 import 'dart:developer' as developer;
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 class QrLoginScreen extends ConsumerStatefulWidget {
   const QrLoginScreen({super.key, String? sessionId});
@@ -19,83 +20,81 @@ class _QrLoginScreenState extends ConsumerState<QrLoginScreen> {
   @override
   void initState() {
     super.initState();
-    // _generateSession();
+    _generateSession();
   }
 
-  // Future<void> _generateSession() async {
-  //   sessionId = _generateRandomId();
-  //   await FirebaseFirestore.instance
-  //       .collection('tv_sessions')
-  //       .doc(sessionId)
-  //       .set({
-  //     'userId': null,
-  //     'createdAt': FieldValue.serverTimestamp(),
-  //   });
-  //
-  //   // Listen for login changes
-  //   FirebaseFirestore.instance
-  //       .collection('tv_sessions')
-  //       .doc(sessionId)
-  //       .snapshots()
-  //       .listen((snapshots) {
-  //     developer.log("userId: ${snapshots.data()?['userId']}");
-  //     if (snapshots.exists && snapshots.data()?['userId'] != null) {
-  //       _qrCodeSignIn(snapshots.data()?['userId']);
-  //     }
-  //   });
-  //
-  //   setState(() {});
-  // }
-  //
-  // String _generateRandomId() {
-  //   final random = Random();
-  //   return List.generate(6, (index) => random.nextInt(10))
-  //       .join(); //generates a 6-digit random id
-  // }
-
-  Future<void> _qrCodeSignIn(String userId) async {
-    developer.log('userid in _qrCodeSignIn: $userId');
-    User? user = FirebaseAuth.instance.currentUser;
-
-    if (user == null) {
-      //   If user not logged in, show Google Sign-In, or email-password authentication;
-      await FirebaseAuth.instance.signInWithProvider(GoogleAuthProvider());
-      user = FirebaseAuth.instance.currentUser;
-    }
-
-    //Update Firestore session with the user's UID
+  Future<void> _generateSession() async {
+    sessionId = Uuid().v4();
+    setState(() {});
     await FirebaseFirestore.instance
         .collection('tv_sessions')
         .doc(sessionId)
-        .update({"uid": user!.uid, "status": "logged_in"});
+        .set({
+      'userId': null,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
 
-    // final doc = await FirebaseFirestore.instance
-    //     .collection('tv_sessions')
-    //     .doc(sessionId)
-    //     .get();
-    //
-    // developer.log("Doc: ${doc.data}");
-    //
-    // if (doc.exists) {
-    //   final customToken = doc.data()?['userId'];
-    //   try {
-    //     final userCredential =
-    //         await FirebaseAuth.instance.signInWithCustomToken(customToken);
-    //
-    //     developer.log("userCredential: ${userCredential.user} ");
-    //
-    //     if (userCredential.user?.uid == userId) {
-    //       if (mounted) {
-    //         context.pushNamed("/home");
-    //       }
-    //     }
-    //   } catch (err) {
-    //     debugPrint("Login Error: $err");
-    //   }
-    // } else {
-    //   debugPrint("No Valid Custom token Found");
-    // }
+    // Listen for login changes
+    FirebaseFirestore.instance
+        .collection('tv_sessions')
+        .doc(sessionId)
+        .snapshots()
+        .listen((snapshots) {
+      developer.log("userId: ${snapshots.data()?['uid']}");
+      if (snapshots.exists && snapshots.data()?['uid'] != null) {
+        String uid = snapshots.data()?['uid'];
+        developer.log('TV Logged in with UID:$uid');
+        context.pushReplacement("/home");
+        // _qrCodeSignIn(snapshots.data()?['uid']);
+      }
+    });
+
+    // setState(() {});
   }
+
+  // Future<void> _qrCodeSignIn(String userId) async {
+  //   developer.log('userid in _qrCodeSignIn: $userId');
+  //   User? user = FirebaseAuth.instance.currentUser;
+  //
+  //   if (user == null) {
+  //     //   If user not logged in, show Google Sign-In, or email-password authentication;
+  //     await FirebaseAuth.instance.signInWithProvider(GoogleAuthProvider());
+  //     user = FirebaseAuth.instance.currentUser;
+  //   }
+  //
+  //   //Update Firestore session with the user's UID
+  //   await FirebaseFirestore.instance
+  //       .collection('tv_sessions')
+  //       .doc(sessionId)
+  //       .update({"uid": user!.uid, "status": "logged_in"});
+  //
+  //   // final doc = await FirebaseFirestore.instance
+  //   //     .collection('tv_sessions')
+  //   //     .doc(sessionId)
+  //   //     .get();
+  //   //
+  //   // developer.log("Doc: ${doc.data}");
+  //   //
+  //   // if (doc.exists) {
+  //   //   final customToken = doc.data()?['userId'];
+  //   //   try {
+  //   //     final userCredential =
+  //   //         await FirebaseAuth.instance.signInWithCustomToken(customToken);
+  //   //
+  //   //     developer.log("userCredential: ${userCredential.user} ");
+  //   //
+  //   //     if (userCredential.user?.uid == userId) {
+  //   //       if (mounted) {
+  //   //         context.pushNamed("/home");
+  //   //       }
+  //   //     }
+  //   //   } catch (err) {
+  //   //     debugPrint("Login Error: $err");
+  //   //   }
+  //   // } else {
+  //   //   debugPrint("No Valid Custom token Found");
+  //   // }
+  // }
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/auth/web_auth.dart
+++ lib/screens/auth/web_auth.dart
@@ -1,11 +1,30 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class WebLoginPage extends StatelessWidget {
+class WebLoginPage extends ConsumerStatefulWidget {
   final String? sessionId;
 
-  const WebLoginPage({this.sessionId, super.key});
+  WebLoginPage({this.sessionId, super.key});
+
+  @override
+  ConsumerState<WebLoginPage> createState() => _WebLoginPageState();
+}
+
+class _WebLoginPageState extends ConsumerState<WebLoginPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+
+  QRViewController? controller;
+
+  bool isPorcessing = false;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,21 +47,35 @@ class WebLoginPage extends StatelessWidget {
                 //   "userId": FirebaseAuth.instance.currentUser?.uid,
                 // });
                 // Navigator.of(context).pop(); // Close the web login page
+                setState(() {
+                  isPorcessing = true;
+                });
+                User? user = FirebaseAuth.instance.currentUser;
+                GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
                 UserCredential userCredential = await FirebaseAuth.instance
-                    .signInWithPopup(GoogleAuthProvider());
+                    .signInWithPopup(googleAuthProvider);
 
-                if (userCredential.user != null) {
-                  String? idToken = await userCredential.user?.getIdToken();
+                user = userCredential.user;
+
+                if (user != null) {
+                  // String? idToken = await userCredential.user?.getIdToken();
 
                   await FirebaseFirestore.instance
                       .collection('tv_sessions')
-                      .doc(sessionId)
-                      .update({'userId': idToken}); // Store ID token
+                      .doc(widget.sessionId)
+                      .update({
+                    'uid': user.uid,
+                    'status': 'logged_in'
+                  }); // Store ID token
 
                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                       content: Text(
                           "Successfully Logged in. You can now return to your TV")));
                 }
+
+                setState(() {
+                  isPorcessing = false;
+                });
               },
               child: Text(
                 "Login with Google",
